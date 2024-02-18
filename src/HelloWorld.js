9ed7a697ea7b0d6d2b8dd9da5e17df238cb02f46
student <student@BA.local>
2024-02-18 12:22:26 +0200

    First task

---
+++ src/HelloWorld.js
@@ -0,0 +1,18 @@
+const HelloWorld = () => {
+
+
+
+    return(
+        <div style={{ 
+            backgroundColor: 'pink',
+            color:'#FF1493',
+            padding: '10px', 
+            borderRadius: '5px',
+            textAlign: 'center',
+            fontSize: '20px'
+          }}>
+<h1>Hello:)</h1>
+        </div>
+    )
+}
+export default HelloWorld;
