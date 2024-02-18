45563198c1e51bdd0e1d4246c677ef83f4102630
student <student@BA.local>
2024-02-18 12:43:18 +0200

    First Task

---
+++ src/Component/tair.js
@@ -0,0 +1,16 @@
+import react from 'react'
+const HelloComponent = () =>{
+    return(
+        <div style={{
+            backgroundColor:'pink',
+            color:'#FF1493',
+            padding:'10px',
+            borderRadius:'5px',
+            textAlign:'center',
+            fontSize:'50px'
+        }}>
+            hello
+        </div>
+    );
+};
+export default HelloComponent;
