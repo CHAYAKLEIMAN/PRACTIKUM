33755de4a2dc8cbda613ed23aed8858767d175b0
student <student@BA.local>
2024-02-18 13:12:24 +0200

    task

---
+++ Components/Eden.js
@@ -0,0 +1,21 @@
+import react from 'react'
+import App from '../../src/App';
+
+const HelloComponent = () => {
+    return (
+      
+      <div style={{
+        backgroundColor: 'pink',
+        color: '',
+      
+        textAlign: 'center',
+        fontSize: '2.5em'
+        
+      }}>
+        <h1>Hello!</h1>
+        
+      </div>
+    );
+  };
+  
+  export default HelloComponent;
