7186c55831174909593d6bc9dbd8330edf230a82
student <student@BA.local>
2024-02-18 13:10:37 +0200

    this is my first project

---
+++ p1.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+export default class Shirnaf extends Component{
+
+render()
+{
+    return(
+    <div>
+        <Text>Hello! </Text>
+    </div>
+    )
+}}
