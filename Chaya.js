b7bdae33cace295f63773cb867c0fcaef39c7140
student <student@BA.local>
2024-02-18 13:21:55 +0200

    jjj

---
+++ Chaya.js
@@ -0,0 +1,22 @@
+import React from "react"
+export default class New extends React.Component{
+
+
+
+
+
+
+
+    render(){
+
+        return(<><div>hello</div>
+
+        </>
+        
+        )
+        
+        
+        
+        
+        }
+        }
